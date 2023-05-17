30dca4494b6500678692898c1c3d2fc43f10a0a8
Caio Fabio <faabio.caio@gmail.com>
2023-05-17 11:02:20 -0300

    updates

--- src/pages/4-useReduce/UseReduce.jsx
+++ src/pages/4-useReduce/UseReduce.jsx
@@ -5,7 +5,7 @@ const UseReduce = () => {
       <p>
         Oferece uma alternativa ao useState quando você precisa lidar com
         estados mais complexos ou com lógica de atualização que depende do
-        estado anterior. É útil para lidar com estados que exigem lógica de
+        estado anterior. É útil pra lidar com estados que exigem lógica de
         atualização mais avançada, como estados com transições de máquina de
         estado.
       </p>
